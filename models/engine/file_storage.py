e7ea9a6623e9f755eedaf7d751a1b1d57e7409d7
mahakamal-e <mahakamal09@gmail.com>
2024-03-18 15:12:50 -0700

    fix pycodestyle

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -52,15 +52,15 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
     def delete(self, obj=None):
         """ Mehod to delete obj from __objects """
         if obj is not None:
-            key = "{}.{}".format(obj.__class__.__name__, obj.id)
-            if key in FileStorage.__objects:
-                del FileStorage.__objects[key]
-            self.save()
+            generate_key = "{}.{}".format(obj.__class__.__name__, obj.id)
 
+            if generate_key in FileStorage.__objects:
+                del FileStorage.__objects[generate_key]
+            self.save()
