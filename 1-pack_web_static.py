2f80c2b3afc369776873de20919420c2a250e503
mahakamal-e <mahakamal09@gmail.com>
2024-04-03 20:28:15 -0700

    edit

---
+++ 1-pack_web_static.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Generates a .tgz archive from the contents of the web_static folder."""
+
+
+from fabric.api import local
+from datetime import datetime
+
+def do_pack():
+    """This function is responsible for generating the .tgz archive."""
+    get_current_date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_file = 'versions'
+    try:
+        local("mkdir -p {}".format(archive_file))
+        archive_name = "{}/web_static_{}.tgz".format(archive_file,
+                                                     get_current_date)
+        return archive_name
+    except Exception as e:
+        return None
