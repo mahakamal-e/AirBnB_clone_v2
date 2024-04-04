7a00a57b1191cd4f10dc61640b52d922b5f61f5b
mahakamal-e <mahakamal09@gmail.com>
2024-04-04 13:42:41 -0700

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,9 +3,8 @@
 from fabric.api import *
 import os
 
-
-env.hosts = ['3.85.1.33', '100.26.171.116']
 env.user = 'ubuntu'
+env.hosts = ['3.85.1.33', '100.26.171.116']
 
 
 def do_deploy(archive_path):
@@ -20,8 +19,8 @@ def do_deploy(archive_path):
         release_dir = "/data/web_static/releases/{}".format(f_without_ext)
 
         run("mkdir -p {}".format(release_dir))
-        run("sudo tar -xzf /tmp/{} -C {}".format(filename, releases_dir))
-        run("sudorm /tmp/{}".format(filename))
+        run("sudo tar -xzf /tmp/{} -C {}".format(filename, release_dir))
+        run("sudo rm /tmp/{}".format(filename))
         run("sudo rm -rf /data/web_static/current")
         run("sudo ln -sf {} /data/web_static/current".format(release_dir))
 
