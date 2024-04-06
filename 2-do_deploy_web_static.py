90fd478a04138691af287e8ee7c91d978543bfcb
mahakamal-e <mahakamal09@gmail.com>
2024-04-06 08:50:29 -0700

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -8,27 +8,28 @@ env.hosts = ['3.85.1.33', '100.26.171.116']
 
 
 def do_deploy(archive_path):
-    """ a Faunction that deplys an archive to web servers."""
+    """Deploys an archive to web servers."""
     if not os.path.exists(archive_path):
         return False
 
     try:
-        filename = archive_path.split("/")[-1]
-        f_without_ext = filename.split(".")[0]
+        filename = os.path.basename(archive_path)
+        f_without_ext = os.path.splitext(filename)[0]
         put(archive_path, "/tmp/")
         run("mkdir -p /data/web_static/releases/{}/".format(f_without_ext))
         run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
             .format(filename, f_without_ext))
         run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/*\
+        run("mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/".format(f_without_ext,
                                                   f_without_ext))
         run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(no_extension))
+            .format(f_without_ext))
         run("rm -rf /data/web_static/current")
         run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(no_extension))
+            .format(f_without_ext))
         return True
 
     except Exception as e:
+        print(e)
         return False
