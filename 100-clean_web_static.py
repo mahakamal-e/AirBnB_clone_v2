ff01ea52cb5145fbf35ad06b5ae72c3cc4904da3
mahakamal-e <mahakamal09@gmail.com>
2024-04-06 16:18:46 -0700

    edit adv task

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -9,9 +9,12 @@ env.hosts = ['3.85.1.33', '100.26.171.116']
 
 
 def do_clean(number=0):
-    """clean unnecessary files"""
+    """ Function that deletes out-of-date archives. """
     releases_path = '/data/web_static/releases'
-    number = 2 if number == '0' else int(number) + 1
+    if number == '0':
+        number = 2
+    else:
+        int(number) + 1
 
     local('cd versions; ls -t | tail -n +{} | xargs rm -rf'
           .format(number))
