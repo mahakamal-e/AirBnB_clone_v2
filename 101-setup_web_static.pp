4b924227c6e65eaffaf2d29978969bdad5d66d73
mahakamal-e <mahakamal09@gmail.com>
2024-04-06 16:09:07 -0700

    edit task adv

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,65 +1,68 @@
-# Define a Puppet class for setting up web_static
-class setup_web_static {
-    package { 'nginx':
-        ensure => installed,
+# Define a Puppet class for setting up web_static.
+
+file{"/data/web_static/releases/test/index.html":
+ensure  => "file",
+content => "hello world",
+owner   => "ubuntu",
+group   => "ubuntu",
+require => File["/data/web_static/releases/test"],}
+
+file{"/data/web_static/releases/test":
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
+require => File["/data/web_static/releases"],}
+
+file{"/data/web_static":
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
+require => File["/data"],}
+
+file{"/data/web_static/releases":
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
+require => File["/data/web_static"],}
+
+file{"/data/web_static/shared":
+ensure  => "directory",
+owner   => "ubuntu",
+group   => "ubuntu",
+require => File["/data/web_static"],}
+
+file{"/data/web_static/current":
+ensure  => "link",
+owner   => "ubuntu",
+group   => "ubuntu",
+target  => "/data/web_static/releases/test",
+require => File["/data/web_static"],}
+
+file{"/data":
+ensure => "directory",
+owner  => "ubuntu",
+group  => "ubuntu",}
+
+file{"/etc/nginx/sites-available/default":
+  ensure  => "file",
+  content => "server {
+    listen 80;
+    listen [::]:80 default_server;
+    server_name magedsaif.tech;
+    location /hbnb_static {
+        alias /data/web_static/current/;
     }
-
-    file { '/data':
-        ensure => directory,
-    }
-
-    file { '/data/web_static':
-        ensure => directory,
-    }
-
-    file { '/data/web_static/releases':
-        ensure => directory,
-    }
-
-    file { '/data/web_static/shared':
-        ensure => directory,
-    }
-
-    file { '/data/web_static/releases/test':
-        ensure => directory,
-    }
-
-    file { '/data/web_static/releases/test/index.html':
-        ensure  => file,
-        content => '<html><head></head><body>Holberton School</body></html>',
-    }
-
-    file { '/data/web_static/current':
-        ensure => link,
-        target => '/data/web_static/releases/test',
-        force  => true,
-    }
-
-    file { '/data':
-        owner   => 'ubuntu',
-        group   => 'ubuntu',
-        recurse => true,
-    }
-
-    file { '/etc/nginx/sites-available/default':
-        ensure  => file,
-        content => "server {
-            listen 80 default_server;
-            listen [::]:80 default_server;
-
-            root /data/web_static/current;
-
-            location /hbnb_static {
-                alias /data/web_static/current/;
-            }
-        }",
-    }
-
-    service { 'nginx':
-        ensure => running,
-        enable => true,
-        require => File['/etc/nginx/sites-available/default'],
-    }
+}",
+  require => Package['nginx'],
+  notify  => Service['nginx'],
+  replace => 'true',
 }
 
-include setup_web_static
+package{ 'nginx':
+  ensure => 'installed',
+}
+
+service{ 'nginx':
+  ensure => 'running',
+  enable => 'true',
+}
