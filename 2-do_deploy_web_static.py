3ee3f768d700b30d1148ea73b349e0480883ccbe
mahakamal-e <mahakamal09@gmail.com>
2024-04-06 09:21:32 -0700

    edit

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,34 +2,32 @@
 """ a Fabric script distributes an archive to your web servers. """
 from fabric.api import *
 import os
+from fabric.contrib import files
+
 
 env.user = 'ubuntu'
 env.hosts = ['3.85.1.33', '100.26.171.116']
 
 
 def do_deploy(archive_path):
-    """Deploys an archive to web servers."""
+    """Function for deploy"""
     if not os.path.exists(archive_path):
         return False
 
-    try:
-        filename = os.path.basename(archive_path)
-        f_without_ext = os.path.splitext(filename)[0]
-        put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}/".format(f_without_ext))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(filename, f_without_ext))
-        run("rm /tmp/{}".format(filename))
-        run("mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/".format(f_without_ext,
-                                                  f_without_ext))
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(f_without_ext))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(f_without_ext))
-        return True
+    data_path = '/data/web_static/releases/'
+    tmp = archive_path.split('.')[0]
+    name = tmp.split('/')[1]
+    dest = data_path + name
 
-    except Exception as e:
-        print(e)
+    try:
+        put(archive_path, '/tmp')
+        run('sudo mkdir -p {}'.format(dest))
+        run('sudo tar -xzf /tmp/{}.tgz -C {}'.format(name, dest))
+        run('sudo rm -f /tmp/{}.tgz'.format(name))
+        run('sudo mv {}/web_static/* {}/'.format(dest, dest))
+        run('sudo rm -rf {}/web_static'.format(dest))
+        run('sudo rm -rf /data/web_static/current')
+        run('sudo ln -s {} /data/web_static/current'.format(dest))
+        return True
+    except:
         return False
