4d45ba76737d2daa1ab42d49b6c77bfa3c337fb8
mahakamal-e <mahakamal09@gmail.com>
2024-04-06 15:49:28 -0700

    edit

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -4,21 +4,16 @@ from fabric.api import *
 import os
 
 
+env.user = "ubuntu"
 env.hosts = ['3.85.1.33', '100.26.171.116']
 
 
 def do_clean(number=0):
-    """clean up old versions"""
-    try:
-        number = int(number)
-        if number < 0:
-            return False
-        if number == 0 or number == 1:
-            number = 1
-        local(f'cd versions; ls -t | tail -n +{number} | xargs rm -rf --')
-        for host in env.hosts:
-            env.host_string = host
-            run(f'cd /data/web_static/releases; ls -t | tail -n +{number} | xargs rm -rf --')
-        return True
-    except Exception as e:
-        return False
+    """clean unnecessary files"""
+    releases_path = '/data/web_static/releases'
+    number = 2 if number == '0' else int(number) + 1
+
+    local('cd versions; ls -t | tail -n +{} | xargs rm -rf'
+          .format(number))
+    run('cd {}; ls -t | tail -n +{} | sudo xargs rm -rf'
+        .format(releases_path, number))
