20c41d457acb71ce5d89af06c11c3914c66d63d7
mahakamal-e <mahakamal09@gmail.com>
2024-04-20 16:51:49 -0700

    flaskll

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-"""Start a Flask web application."""
-
+"""script that starts a Flask web application"""
 from flask import Flask, render_template
-from models import *
+
 from models import storage
 
 app = Flask(__name__)
+app.url_map.strict_slashes = False
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    """Displays an HTML page with a list of all states & related cities."""
+@app.route('/cities_by_states')
+def states():
+    """Get all state data"""
     states = storage.all("State")
-    return render_template("8-cities_by_states.html", states=states)
+    return render_template("8-cities_by_states.html",
+                           states=states)
 
 
 @app.teardown_appcontext
-def teardown(exc):
-    """Close SQLAlchemy session."""
+def terminate(exc):
+    """Close SQLAlchemy session"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
