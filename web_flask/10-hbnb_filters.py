eab73d20a1b8dda2ebd10b1f11278d4986335b62
mahakamal-e <mahakamal09@gmail.com>
2024-04-21 09:11:40 -0700

    ee

--- web_flask/10-hbnb_filters.py
+++ web_flask/10-hbnb_filters.py
@@ -8,6 +8,15 @@ from models.state import State
 app = Flask(__name__)
 
 
+@app.route('/hbnb_filters', strict_slashes=False)
+def states():
+    """display a HTML page"""
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template('10-hbnb_filters.html', states=states,
+                           amenities=amenities)
+
+
 @app.teardown_appcontext
 def teardown(exception):
     """Close SqlAlqemy session"""
